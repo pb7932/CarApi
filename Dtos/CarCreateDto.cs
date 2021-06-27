02d82b3738d382012d5e7047581036328562df6b
pb7932 <pebe1928@gmail.com>
2021-06-27 10:00:52 +0200

    Adding all car dtos

---
+++ Dtos/CarCreateDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CarApi.Dtos
+{
+    public class CarCreateDto
+    {
+        [Required]
+        [MaxLength(200)]
+        [MinLength(2)]
+        public string name { get; set; }
+
+        [Required]
+        public int price { get; set; }
+
+        [Required]
+        public int power { get; set; }
+    }
+}
---
+++ Dtos/CarReadDto.cs
@@ -0,0 +1,11 @@
+namespace CarApi.Dtos
+{
+    public class CarReadDto
+    {
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public int price { get; set; }
+    }
+}
